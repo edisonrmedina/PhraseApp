dbcad44c900588528bd5852532e4ce2e0623988d
rein801206_Siigo <edison1282015@gmail.com>
2023-05-24 18:19:11 -0500

    fix designs

--- src/Components/PhraseComponent.jsx
+++ src/Components/PhraseComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import phrases from '../assets/data/famousPhrases.json';
+import '../Components/PhraseComponente.css'
 
 export default function PhraseComponent() {
 
@@ -31,20 +32,21 @@ export default function PhraseComponent() {
   };
 
   return (
-    <div
-      className="flex items-center justify-center min-h-screen transition-colors"
-      style={{ backgroundColor }}
-    >
+    <div className="flex flex-col items-center justify-center min-h-screen background-image">
       <div className="text-center">
-        <p className='text-black'>{author}</p>
-        <p>- {phrase}</p>
+      <div className="absolute w-443.26 h-88 left-422 top-95"></div>
+        <p className="font-fredoka-one font-normal text-3xl leading-11 text-white text-center">Galletas de la fortunas</p>
+        <p className="text-black">{author}</p>
+        <p class="p-8">{phrase}</p>
         <button
           onClick={changePhrase}
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          className="mt-4 px-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-700 p-"
         >
           Cambiar frase
         </button>
       </div>
+      <div className="image-container-semillas"></div>
     </div>
   );
+  
 }
